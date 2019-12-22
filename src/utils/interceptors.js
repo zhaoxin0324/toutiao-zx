ac99e638372330ef0e22f7feeec1d95c061b585f
zhaoxin <zhaoxin0324@qq.com>
2019-12-22 16:42:23 +0800

    黑马头条PC-统一处理大数字类型

--- src/utils/interceptors.js
+++ src/utils/interceptors.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import router from '../router'
 import { Message } from 'element-ui' // 引入提示信息
 // 配置固定路径
+import JSONBig from 'json-bigint'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 
 // 请求拦截
@@ -14,11 +15,15 @@ axios.interceptors.request.use(function (config) {
   // 请求失败时的处理
   return Promise.reject(error)
 })
-
+// 在响应拦截之前要处理下id失真问题
+axios.defaults.transformResponse = [function (data) {
+  return JSONBig.parse(data) // 用JSONbig.pase替换 JSON.parse 保证数字的正确
+}]
 // 设置响应拦截
 axios.interceptors.response.use(function (response) {
   // response 响应数据
 //   解构一层响应数据
+  console.log(response)
   return response.data ? response.data : {}
 }, function (error) {
   // 状态码不是 200/201/204 时进入此函数
@@ -28,7 +33,7 @@ axios.interceptors.response.use(function (response) {
   let message = '' // 提示信息
   switch (status) {
     case 400:
-      message = '手机号或者验证码错误'
+      message = '请求参数错误'
       break
     case 403:
       window.localStorage.removeItem('userInfo')
--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,26 +1,81 @@
 <template>
-  <el-card>
-      <!-- 引用面包屑组件 -->
-      <!-- 卡片头部组件 -->
-      <bread-crumb slot="header"><!-- 卡片头部组件的插槽 -->
-        <template slot="title">评论列表</template>  <!-- 面包屑组件的具名插槽 -->
-      </bread-crumb>
-      <el-table>
-          <el-table-column label="标题" width="500"></el-table-column>
-          <el-table-column label="评论状态"></el-table-column>
-          <el-table-column label="总评论数"></el-table-column>
-          <el-table-column label="粉丝评论数"></el-table-column>
-          <el-table-column label="操作"></el-table-column>
-      </el-table>
+  <el-card v-loading="loading">
+    <!-- 引用面包屑组件 -->
+    <!-- 卡片头部组件 -->
+    <bread-crumb slot="header">
+      <!-- 卡片头部组件的插槽 -->
+      <template slot="title">评论列表</template>
+      <!-- 面包屑组件的具名插槽 -->
+    </bread-crumb>
+    <el-table :data="list">
+      <el-table-column label="标题" width="500" prop="title"></el-table-column>
+      <el-table-column :formatter="formatterBool" label="评论状态" prop="comment_status"></el-table-column>
+      <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
+      <el-table-column label="粉丝评论数" prop="fans_comment_count"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="obj">
+          <el-button type="text" size="small">修改</el-button>
+          <el-button type="text" size="small" @click="changeStatus(obj.row)">{{ obj.row.comment_status ? '关闭评论' : '打开评论' }}</el-button>
+        </template>                                             <!-- 将每行数据的传入方法中 -->
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    changeStatus (row) {
+      // 需要用到状态类型,obj.row.comment_status 传递参数 根据当前状态获取需要改成的状态
+      let mess = row.comment_status ? '关闭' : '打开'
+      // 提示信息
+      // console.log(row.id.toString()) // id被编辑成了数组 可用tostring方法转回数字
+      // console.log(this)  $confirm遵循promise对象规则
+      this.$confirm(`你确定要${mess}评论么？`).then(() => {
+        // 确定之后发送请求
+        this.$axios({
+          method: 'put',
+          url: '/comments/status',
+          params: { article_id: row.id.toString() },
+          data: { allow_comment: !row.comment_status }
+        }).then((res) => {
+          // 提示信息
+          this.$message({
+            type: 'success',
+            message: '操作成功'
+          })
+          // 调用评论 刷新
+          this.getComment()
+        })
+      })
+    },
+    getComment () {
+      this.loading = true
+      this.$axios({
+        url: '/articles',
+        params: { response_type: 'comment' }
+      }).then(res => {
+        console.log(res)
+        this.list = res.data.results // 获取评论列表数据给本身data
+        this.loading = false
+      })
+    },
+    // 定义布尔值转换方法
+    formatterBool (row, column, cellValue, index) {
+      // cellValue 当前单元格的值
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    this.getComment()
+  }
 }
 </script>
 
 <style>
-
 </style>
