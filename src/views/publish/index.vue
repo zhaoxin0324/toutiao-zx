ed3da735db4b6adba355a89fefd66389510fab58
zhaoxin <zhaoxin0324@qq.com>
2019-12-25 15:47:12 +0800

    黑马头条PC-发表文章-页面结构-简单实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>发表文章</div>
+    <el-card>
+        <bread-crumb slot="header">
+            <template slot="title">发布文章</template>
+        </bread-crumb>
+        <el-form label-width="60px" style="margin-left:40px">
+            <el-form-item label="标题">
+                <el-input style="width:40%"></el-input>
+            </el-form-item>
+            <el-form-item label="内容">
+                <el-input type='textarea' :rows="4"></el-input>
+            </el-form-item>
+            <el-form-item label="封面">
+                <el-radio-group>
+                    <el-radio>单图</el-radio>
+                    <el-radio>三图</el-radio>
+                    <el-radio>无图</el-radio>
+                    <el-radio>自动</el-radio>
+                </el-radio-group>
+            </el-form-item>
+            <el-form-item label="频道">
+                <el-select></el-select>
+            </el-form-item>
+        </el-form>
+    </el-card>
 </template>
 
 <script>
