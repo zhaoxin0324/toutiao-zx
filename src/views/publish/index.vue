aac60c4b27ba3e90e493bb3d82753879a581c3d3
zhaoxin <zhaoxin0324@qq.com>
2019-12-26 16:30:26 +0800

    黑马头条PC-发表文章-发布文章检验逻辑实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -3,23 +3,23 @@
         <bread-crumb slot="header">
             <template slot="title">发布文章</template>
         </bread-crumb>
-        <el-form label-width="60px" style="margin-left:40px">
-            <el-form-item label="标题">
-                <el-input style="width:40%"></el-input>
+        <el-form label-width="60px" style="margin-left:40px" :model="formData" :rules="publishRules">
+            <el-form-item label="标题" prop="title">
+                <el-input style="width:40%" v-model="formData.title"></el-input>
             </el-form-item>
-            <el-form-item label="内容">
-                <el-input type='textarea' :rows="4"></el-input>
+            <el-form-item label="内容" prop="content">
+                <el-input type='textarea' :rows="4" v-model="formData.content"></el-input>
             </el-form-item>
-            <el-form-item label="封面">
-                <el-radio-group>
+            <el-form-item label="封面" prop="type">
+                <el-radio-group v-model="formData.cover.type">
                     <el-radio :label="1">单图</el-radio>
                     <el-radio :label="3">三图</el-radio>
                     <el-radio :label="0">无图</el-radio>
                     <el-radio :label="-1">自动</el-radio>
                 </el-radio-group>
             </el-form-item>
-            <el-form-item label="频道">
-                <el-select>
+            <el-form-item label="频道" prop="channel_id">
+                <el-select v-model="formData.channel_id" >
                     <el-option  v-for="item in channels" :key="item.id"
                     :value="item.id" :label="item.name"></el-option>
                 </el-select>
@@ -38,8 +38,21 @@ export default {
     return {
       channels: [],
       formData: {
-
-      }
+        title: '', // 标题
+        content: '', // 内容
+        cover: {
+          type: 0, // 封面内容
+          images: [] // 存储图片地址
+        },
+        channel_id: null // 频道id
+      },
+      publishRules: {
+        title: [{ required: true, message: '输入标题' }, {
+          min: 5, max: 30, message: '5-30字符'
+        }],
+        content: [{ required: true, message: '输入内容' }],
+        channel_id: [{ required: true, message: '选择频道' }]
+      } // 校验规格对象
     }
   },
   methods: {
