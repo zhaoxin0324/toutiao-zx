1345b70468c9dac0e4fdd5ff74333fefa0af0187
zhaoxin <zhaoxin0324@qq.com>
2019-12-27 18:03:57 +0800

    黑马头条PC-账户信息-数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -6,16 +6,16 @@
       <!-- 左侧表单组件 并且需要手动校验-->
       <el-form label-width="60px" style="margin-left:100px">
           <el-form-item label="用户名">
-              <el-input style="width:25%"></el-input>
+              <el-input style="width:25%" v-model="formData.name"></el-input>
           </el-form-item>
           <el-form-item label="简介">
-              <el-input style="width:25%"></el-input>
+              <el-input style="width:25%" v-model="formData.intro"></el-input>
           </el-form-item>
           <el-form-item label="邮箱">
-              <el-input style="width:25%"></el-input>
+              <el-input style="width:25%" v-model="formData.email"></el-input>
           </el-form-item>
           <el-form-item label="手机号">
-              <el-input style="width:25%"></el-input>
+              <el-input style="width:25%" v-model="formData.mobile" disabled=""></el-input>
           </el-form-item>
       </el-form>
       <!-- 右侧上传图片组件 -->
@@ -27,7 +27,30 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      formData: {
+        name: '', // 用户名
+        intro: '', // 简介
+        photo: '', // 头像
+        email: '', // 邮箱
+        mobile: '' // 手机号
+      }
+    }
+  },
+  methods: {
+    // 获取用户信息
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(res => {
+        this.formData = res.data
+      })
+    }
+  },
+  created () {
+    this.getUserInfo()
+  }
 }
 </script>
 
