7ea207da331efafabe5460705b85eac02f8ac167
zhaoxin <zhaoxin0324@qq.com>
2019-12-28 17:44:24 +0800

    黑马头条PC-页面进度条引入应用

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,7 +1,10 @@
 // 设置路由全局守卫 router.beforeEach(function(to,from,next){}) 监视
 // 首先引入路由文件index.js
 import router from '../router'
+import progress from 'nprogress' // 引入进度条组件
+import 'nprogress/nprogress.css'
 router.beforeEach(function (to, from, next) {
+  progress.start() // 开启进度条
   if (to.path.startsWith('/home')) {
     let token = window.localStorage.getItem('userInfo')
     if (token) {
@@ -12,4 +15,7 @@ router.beforeEach(function (to, from, next) {
   } else {
     next()
   }
+})
+router.afterEach(function () {
+  progress.done()
 })
